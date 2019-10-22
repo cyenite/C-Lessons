13287bcdeeda94475286651eb2719b0822dac76d
Akron254 <aaronrono42@gmail.com>
2019-10-22 22:21:28 +0300

    Added Interfaces and Generics

---
+++ Generics/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Generics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Operations<int> math = new Operations<int>();
+            math.Assign(3);
+
+            math.FindType();   
+        }
+    }
+    class Operations<T>
+    {
+        private T name;
+
+        public void Assign(T value){
+           
+           
+            
+        }
+        public void FindType()
+        {
+            System.Console.WriteLine("Data type: {0}  Value: {1}",typeof(T).ToString(), name);
+        }
+    }
+}
---
+++ Interfaces/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace Interfaces
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IAccount ian = new Premium();
+            ian.yOB = 2000;
+
+            System.Console.WriteLine(ian.ageCalc());
+        }
+    }
+
+    interface IAccount
+    {
+        string name { get; set; }
+        int yOB {get; set;}
+        int accNo{get; set;}
+        int ageCalc();
+
+    }
+
+    interface Itrial
+    {
+        string name { get; set; }
+        int accNo{get; set;}
+
+    }
+
+    class Premium: IAccount , Itrial
+    {
+        public string name {get; set;}
+        public int yOB {get; set;}
+        public int accNo{get; set;}
+        public int ageCalc()
+        {
+            int age =2019 - yOB;
+            return age;
+        }
+    }
+
+    class Free: IAccount
+    {
+        public string name {get; set;}
+        public int yOB {get; set;}
+        public int accNo{get; set;}
+        public int ageCalc()
+        {
+            int age =2019 - yOB;
+            return age;
+        }
+    }
+
+}
